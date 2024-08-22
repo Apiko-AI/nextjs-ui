ba6206ccac3ff98dac45194b50a59153a1329578
Volodymyr Tymets <volodymyrtymets@gmail.com>
2024-08-22 11:27:02 +0300

    Adapt to use fast api, add chat history, and apiko ui  (#3)
    * add eslint configs
    
    * rewrite rout - POST
    
    * refactor to apiko design

---
+++ src/types/chains.ts
@@ -0,0 +1,6 @@
+export enum ChainEnum {
+  FreeSQLModel = "SQL-model-without-suggestion",
+  SQLWithSuggestion = "SQL-model-with-suggestion",
+  CombineSQLAndSuggestion = "combine-SQL-model-and-suggestion",
+  WithMessageHistory = "with-message-history",
+}
