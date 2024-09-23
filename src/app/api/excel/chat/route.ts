63479103c10f663d00fac6961ea82970896aaeda
vova <volodymyrtymets@gmail.com>
2024-09-23 14:04:50 +0300

    add excel page

---
+++ src/app/api/excel/chat/route.ts
@@ -0,0 +1,18 @@
+export const runtime = "edge";
+export const dynamic = "force-dynamic";
+
+export async function POST(req: Request) {
+  const { messages, chatId } = await req.json();
+
+  return fetch(`${process.env.AI_API_URL}/excel/chat`, {
+    method: "POST",
+    body: JSON.stringify({
+      messages,
+      chat_id: chatId,
+    }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+}
